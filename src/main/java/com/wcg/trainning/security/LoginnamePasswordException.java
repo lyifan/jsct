d6466bbb24281a8086308f0e6ec0d0af062b146c
yifan liu <yifanliu.uk@gmail.com>
2015-10-18 09:16:22 +0100

    authz ready 2

---
+++ src/main/java/com/wcg/trainning/security/LoginnamePasswordException.java
@@ -0,0 +1,10 @@
+package com.wcg.trainning.security;
+
+public class LoginnamePasswordException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5874996959537802114L;
+
+}
---
+++ src/main/java/com/wcg/trainning/security/OTPException.java
@@ -0,0 +1,10 @@
+package com.wcg.trainning.security;
+
+public class OTPException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1235798179777525198L;
+
+}
